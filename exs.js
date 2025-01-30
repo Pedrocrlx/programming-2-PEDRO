f687cac9a46c0b87130dfac3d2e882b963e34e50
Pedro Santos <pedrosantoscrlx@gmail.com>
2025-01-30 21:58:53 +0000

    doing 1ex

---
+++ exs.js
@@ -0,0 +1,12 @@
+/* Write a function findDuplicates(arr) that returns an array of duplicate elements. */
+
+const arr = [1, 2, 3, 3, 5];
+
+function findDuplicates(arr) {
+    for (let i = 0; i < arr.lenght; i++) {
+        for (let j = 0; j < arr.lenght; j++) {
+        }
+    }
+}
+
+findDuplicates(arr)
--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function hora(time) {
+console.time("bigO");
+
+const n = 50;
+for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= n; j++) {
+        console.log(i + j);
+    }
+}
+/* function hora(time) {
     if (time <= 12) {
         console.info("Bom dia jorge");
     }
@@ -8,8 +16,9 @@ function hora(time) {
     else {
         console.info("Boa noite jorge");
     }
-}
+} */
 //hora(20) 
 
-let nota = 20
-console.log(nota < 9? "passou" : "não passou")
+/* let nota = 20
+console.log(nota < 9 ? "passou" : "não passou") */
+console.timeEnd("bigO");
