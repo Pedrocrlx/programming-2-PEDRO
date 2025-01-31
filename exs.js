feabfc9ee232b922135004f9136287ed573cb529
pedro <pedrosantoscrlx@gmail.com>
2025-01-31 00:22:06 +0000

    almost finish ex1

--- exs.js
+++ exs.js
@@ -1,12 +1,22 @@
 /* Write a function findDuplicates(arr) that returns an array of duplicate elements. */
 
 const arr = [1, 2, 3, 3, 5];
+let duplicateArray = [];
 
 function findDuplicates(arr) {
-    for (let i = 0; i < arr.lenght; i++) {
-        for (let j = 0; j < arr.lenght; j++) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                if (!duplicateArray.includes(arr[i])) {
+                    duplicateArray.push(arr[i]);
+                }
+            }
         }
-    }
-}
+    } return console.log(duplicateArray);
+} 
 
-findDuplicates(arr)
+function Test(arr) {
+    console.log(arr.length);
+}
+Test(arr)
+findDuplicates();
