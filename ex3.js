6ddef429c5a681edb56a70cd95885405b6edab36
pedro <pedrosantoscrlx@gmail.com>
2025-02-02 18:22:13 +0000

    session:1 ex3 done✅

--- ex3.js
+++ ex3.js
@@ -1,3 +1,5 @@
+const { inflate } = require("zlib");
+
 /* Example 1:
 
 Input: nums = [2,7,11,15], target = 9
@@ -12,20 +14,35 @@ Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 */
-
 function twoSum() {
-    const nums = [2, 7, 11, 15];
-    const [first, second] = nums;
-    
-    // let result = nums;
-    // const target = 9;
+    let index = {}; // Object to save index as a KEY and numbers as VALUES. 
+    const nums = [2, 5, 11, 15]
+    const target = 26;
+    let solved = null;
 
-    // if (target == result) {
-    //     console.log(result);
-    // }
+    // Loop through the array and create a hash map with index as a KEY and numbers as VALUES.
+    for (let i = 0; i < nums.length; i++) {
+
+        let findIndex = target - nums[i];
+        if (findIndex in index) {
+            console.info([index[findIndex], i]);
+        }
+        index[nums[i]] = i;
+        for (let s = 0; s < i; s++) {
+            if (nums[i] + nums[s] === target) {
+                solved = nums[s] + nums[i];
+                console.info(`${nums[s]}` + " + " + `${nums[i]}` + ' = ' + solved);
+            }
+        }
+    }
+
+    // If solved dont have any solution will return null.
+    // console.info(solved);
+
+    // But i dont want null :)
+    if (solved === null) {
+        console.info("For this target dont have a solution! ");
+    }
 }
 
-twoSum()
-
-
-
+twoSum() 
