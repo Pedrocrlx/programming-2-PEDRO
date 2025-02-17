6ae21cedda16d4de92441f96ddd5371f72e462ba
Pedrocrlx <100964693+Pedrocrlx@users.noreply.github.com>
2025-02-17 22:00:56 +0000

    session-12: done

---
+++ session-12.js
@@ -0,0 +1,39 @@
+/* function* evenNumbers(array) {
+    for (const even of array) {
+        if (even % 2 == 0) {
+            yield even;
+        }
+    }
+}
+
+const generator = evenNumbers([2, 4, 6, 8, 9]);
+
+let count = 0;
+for (const num of generator) {
+    console.log(num);
+    count++;
+    if (count === 3) break; // Interrompe após 3 números pares
+}
+ */
+//CHALLENGE
+
+/* function* endStart(start, end) {
+    for (let i = 0; i <= end; i++) {
+        if (start <= end) {
+            yield start;
+        }
+    }
+}
+const result = endStart(10, 20);
+
+console.log(result.next().value);
+ */
+function* startEnd(start, end) {
+    for (let i = 1; i <= start || i <= end; i++) {
+        yield console.log(i);
+    }
+}
+
+const challenge = startEnd(5, 10);
+
+console.log(challenge.next().value);
