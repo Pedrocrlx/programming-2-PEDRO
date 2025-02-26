6bb7659c90581dc5951db6e06a0cc99d72f10355
Pedro <pedrosantoscrlx@gmail.com>
2025-02-26 21:01:00 +0000

    update index.js

--- FinalProject/src/index.js
+++ FinalProject/src/index.js
@@ -1,56 +1,42 @@
 import process from "node:process";
 import fs from "node:fs";
-import { fetchRecipes } from "./recipes.js";
-
-/**
- * Creates a directory or throws an error if the directory already exists.
- * @param {string} directory - The name of the directory to create.
- * @throws {Error} Throws an error if the directory already exists.
- */
-const createDirectoryOrThrow = (directory) => {
-    if (fs.existsSync(directory)) {
-        throw new Error(
-            `Directory ${directory} already exists. Please use a different name or remove the existing directory.`,
-        );
-    }
-    fs.mkdirSync(directory, { recursive: true });
-};
-
-/**
- * Truncates a string to a specified length and adds ellipsis if necessary.
- * @param {string} str - The string to truncate.
- * @param {number} maxLength - The maximum length of the string.
- * @returns {string} The truncated string.
- */
-const truncateString = (str, maxLength) => {
-    return str.length > maxLength
-        ? `${str.substring(0, maxLength - 3)}...`
-        : str;
-};
+import { fetchRecipes } from "./fetchRecipes.js";
+import { displayHelp } from "./helpFlag.js";
+import { truncateString } from "./truncateString.js";
+import { createDirectoryOrThrow } from "./createDirectory.js";
 
 /**
  * The main function that fetches and displays recipes based on user input.
  * If the `--save` flag is provided, it saves the recipes to markdown files in a folder named after the query.
  * @async
  * @function main
+ * @constant saveFlag --save argument to save recipe.
+ * @constant helpFlag --help argument to help.
  * @returns {Promise<void>} A promise that resolves when the function completes.
  */
 const main = async () => {
     const query = process.argv[2];
     const saveFlag = process.argv.includes("--save");
+    const helpFlag = process.argv.includes("--help");
 
     if (!query) {
         console.log("Please provide an ingredient or recipe query.");
+    }
+
+    if (query) console.log(`Searching for recipes with: ${query}`);
+
+    const recipesData = await fetchRecipes(query);
+
+    if (helpFlag) {
+        displayHelp();
         return;
     }
 
-    console.log(`Searching for recipes with: ${query}`);
-    const recipesData = await fetchRecipes(query);
     if (recipesData) {
         const recipesTable = recipesData.results.map((recipe, index) => ({
             Recipe: index + 1,
             Name: truncateString(recipe.name, 50),
-            Description: truncateString(recipe.description, 70),
+            Description: truncateString(recipe.description, 110),
         }));
         console.table(recipesTable);
 
@@ -66,12 +52,8 @@ const main = async () => {
                         `${baseDirectory}/Recipe_${index + 1}.md`,
                         recipeMarkdown,
                     );
-                    console.log(
-                        `Recipe ${index + 1} saved to ${baseDirectory}/Recipe_${
-                            index + 1
-                        }.md`,
-                    );
                 });
+                console.log(`All recipes saved to ${baseDirectory}`);
             } catch (error) {
                 console.error(error.message);
                 return;
